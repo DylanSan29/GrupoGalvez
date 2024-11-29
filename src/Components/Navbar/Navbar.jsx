cf42d0e736e059e76264bd2ce80d5cd42fcf1a7c
Dylan Sanchez <dylansanchez2902@gmail.com>
2024-11-28 20:22:08 -0600

    Mejoras responsivas

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // Estado para controlar la apertura/cierre del menú
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen); // Alternar el estado del menú
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <a href="/">Logo</a>
       </div>
-      <ul className="navbar-links">
+      {/* Botón hamburguesa */}
+      <button className="navbar-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+      <ul className={`navbar-links ${isMenuOpen ? "active" : ""}`}> {/* Condicionalmente agregar la clase 'active' */}
         <li><a href="/">Inicio</a></li>
         <li><a href="/FeaturesSection">Características</a></li>
         <li><a href="/AboutUs">Sobre Nosotros</a></li>
@@ -15,9 +25,10 @@ const Navbar = () => {
         <li><a href="/Contact">Contacto</a></li>
       </ul>
       <div className="navbar-cta">
+        {/* Botón o contenido adicional */}
       </div>
     </nav>
-  );  
+  );
 };
 
 export default Navbar;
